afcf4f4e4c81e4b63f593ba1f656d101645e5b5f
yarodilsya <karina.simonenko.2004@bk.ru>
2024-03-05 22:10:53 +0700

    создан первый коммит

---
+++ hello_world.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "H";
+    cout << "e";
+    cout << "l";
+    cout << "l";
+    cout << "o";
+    cout << " ";
+    cout << "w";
+    cout << "o";
+    cout << "r";
+    cout << "l";
+    cout << "d";
+    cout << endl;
+
+    return 0;
+}
