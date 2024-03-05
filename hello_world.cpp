814576f555ddc32b210d42958aa7e22af11a691e
yarodilsya <karina.simonenko.2004@bk.ru>
2024-03-06 00:37:15 +0700

    Добавлены комментарии

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,9 +5,9 @@ int main() {
     std::cout << "Enter your name: ";
 
     std::string name;
-    std::getline(std::cin, name);
+    std::getline(std::cin, name); //ввод имени
 
-    std::cout << "Hello world from @" << name << std::endl;
+    std::cout << "Hello world from @" << name << std::endl; //вывод имени
 
     return 0;
 }
